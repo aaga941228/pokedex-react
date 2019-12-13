7bbb4c136021e4058cf293a56749f6b9edf91464
aaga <aaga941228@outlook.es>
2019-12-13 16:27:04 -0600

    lc

--- src/components/PokemonView.jsx
+++ src/components/PokemonView.jsx
@@ -8,7 +8,7 @@ const PokemonView = props => {
   const { pokemon } = props;
 
   return (
-    <div className="card col-10 col-md-8 col-lg-6 col-xl-4 my-5 p-3 mx-auto bg-secondary rounded">
+    <div className="card col-10 col-md-8 col-lg-6 col-xl-4 my-5 p-3 mx-auto bg-secondary rounded shadow">
       <div className="card bg-transparent my-1 p-1">
         <div className="card-title p-3">
           <p className="fs-lg-1-25">#{pokemon.id}</p>
